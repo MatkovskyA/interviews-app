b85d2f9cdcfe5a3d102aa8c14cbb1b3ff4f7f88d
MatkovskyA <a.matkovsky14@gmail.com>
2024-10-19 20:16:56 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import AppHeader from '@/components/AppHeader/AppHeader.vue'
+</script>
+
+<template>
+  <div class="container">
+    <AppHeader />
+    <div class="content">
+      <RouterView />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  max-width: 1280px;
+  margin: auto;
+  padding: 20px;
+}
+
+.content {
+  height: 100vh;
+}
+</style>
